23277af7976a0894306e3ae7c4d346f9bc92cf53
Jewwst <pegasus_hurcules@yahoo.com>
2024-02-21 13:21:16 -0600

    fetched data from api using useEffect

--- src/components/contactlist.jsx
+++ src/components/contactlist.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ContactRow from "./contactrow"
 import { useState } from "react"
+import {useEffect} from "react"
 export const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
@@ -9,6 +10,20 @@ export const dummyContacts = [
 export default function ContactList(){
     const [contacts, setContacts] = useState(dummyContacts)
   console.log("Contacts: ", contacts)
+  useEffect(()=>{
+    async function fetchContacts(){
+        try {
+            const response = await fetch(
+              "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"  
+            )
+            const result = await response.json()
+            setContacts(result)
+            } catch (error) {
+        console.error(error)
+    }
+}
+fetchContacts()
+  },[])
     return (
         <table>
             <thead>
